4d83c90315cdb20f42d4228058b5ec62822c3554
jeremyohandley <32361537+jeremyohandley@users.noreply.github.com>
2020-06-08 20:02:00 -0400

    implemented basic ui

--- app/src/main/java/com/ohandley/jeremy/watch/MainActivity.kt
+++ app/src/main/java/com/ohandley/jeremy/watch/MainActivity.kt
@@ -2,11 +2,15 @@ package com.ohandley.jeremy.watch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar!!.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        supportActionBar!!.setCustomView(R.layout.custom_action_bar)
+
     }
 }
